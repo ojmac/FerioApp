858a067ff0f91ca9892afb751b96cbac68d93dd9
Ojmacmac <ojmacmac@gmail.com>
2025-04-21 14:19:56 +0200

    Actualizando .gitignore y eliminando archivos bloqueados

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace FerioApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Método que se ejecuta cuando se hace clic en el botón
+        private async void OnNavigateButtonClicked(object sender, EventArgs e)
+        {
+            // Aquí se navega a UsuarioPage usando Shell
+            await Shell.Current.GoToAsync(new UsuarioPage());
+        }
+    }
+}
---
+++ UsuarioPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Maui.Controls;
+using System;
+
+
+namespace FerioApp.Models
+{
+    public partial class UsuarioPage : ContentPage
+    {
+        private readonly ApiService _apiService;
+
+        public UsuarioPage(ApiService apiService)
+        {
+            InitializeComponent();
+            _apiService = apiService;
+        }
+
+        // Se ejecutará cuando la página esté completamente cargada
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            // Obtener información del usuario con ID 5
+            var usuarioInfo = await _apiService.GetUsuarioAsync(5);
+
+            // Mostrar la información en el Label
+            UsuarioLabel.Text = usuarioInfo;
+        }
+    }
+}
